51cea4df556210c925b5e3bc51223907fa1b0563
Dillon Townsend <dtownsend@contentactive.com>
2023-07-27 07:25:58 -0500

    blah

---
+++ backup_and_build.sh
@@ -0,0 +1,64 @@
+# Function to display error messages
+function error {
+    echo "Error: $1"
+    exit 1
+}
+
+# Function to backup the current folder
+function backup_folder {
+    local date=$(date '+%Y-%m-%d-%H:%M:%S')
+
+    local backup_folder="bak"
+
+    local backup_dir="$backup_folder/$date"
+
+    mkdir -p $backup_dir || error "Failed to create the backup directory"
+
+    for file in *; do
+      if [ "$file" != "$backup_dir" ] && [ "$file" != "backup_and_build.sh" ] && [ "$file" != "Web.config" ]; then
+        mv "$file" "$backup_dir" || error "Failed to move files to the backup folder"
+      fi
+    done
+
+    echo "Backup created successfully."
+}
+
+# Function to pull changes and build the application
+function pull_and_build {
+    local branch_name="$1"
+
+    git checkout "$branch_name" || error "Failed to switch to $branch_name branch"
+    git pull origin "$branch_name" || error "Failed to pull changes from $branch_name branch"
+    
+    # Assuming you use npm for building the React project
+    npm install || error "Failed to install npm packages"
+    npm run build || error "Failed to build the application"
+}
+
+# Main script
+
+# Ask the user for the branch they want to update
+read -p "Which branch would you like to pull and build? (development/staging/production): " branch_name
+
+git fetch || error "Failed to fetch changes from the remote repository"
+
+# Check if the current branch is tracking a remote branch
+if [ -n "$(git ls-remote --exit-code origin "$branch_name")" ]; then
+  # Compare the local and remote branches to check for any differences
+  git fetch
+  if git diff HEAD..origin/"$branch_name" --exit-code; then
+    echo "No changes in the remote repository."
+    exit 1
+  else
+    # Backup the current folder
+    backup_folder
+
+    # Update the chosen branch
+    pull_and_build "$branch_name"
+  fi
+else
+  echo "The current branch is not tracking a remote branch. Please configure tracking."
+  exit 1
+fi
+
+echo "Update process completed successfully."
