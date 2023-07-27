2f0c71912d5fd217b02ee238bec2a57197cd07cd
dtownsendca <140512026+dtownsendca@users.noreply.github.com>
2023-07-27 07:52:55 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React TEST 7
+          Learn React TEST 8
         </a>
       </header>
     </div>
